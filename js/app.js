f5149653f27520460a860761708d1bc3852ab934
Michael Blumenthal <bloomer25@googlemail.com>
2018-09-06 15:06:57 +0100

    initial commit, finishes app

---
+++ js/app.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleForm);
+  const dButton = document.querySelector('#delete-all');
+  dButton.addEventListener('submit', deleteAll);
+})
+
+const handleForm = function(event) {
+  event.preventDefault();
+  const title = event.target.title.value;
+  const author = event.target.author.value;
+  const category = event.target.category.value;
+
+  const newBook = document.createElement('p');
+  newBook.textContent = `Title: ${title}, Author: ${author}, Category: ${category}`;
+  newBook.classList.add('bookItem')
+  const info = document.querySelector('#reading-list');
+  info.appendChild(newBook);
+  this.reset();
+}
+
+const deleteAll = function() {
+  const allElements = document.querySelectorAll('.bookItem')
+  while(allElements.firstChild){
+    allElements.removeChild(allElements.firstChild);
+  }
+}
